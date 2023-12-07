88da016c926fd722aa4db2a6fb10de76138aab4f
MdMuzahid07 <muzahid7396@gmail.com>
2023-12-07 14:52:57 +0600

    UI breaking issue fixed and hire button in header

--- src/App.jsx
+++ src/App.jsx
@@ -11,38 +11,49 @@ const App = () => {
 
           <div
             id="home"
-            className={`${styles.paddingX} bg-primary min-h-screen w-full flex-col flex items-center justify-center text-white`}
+            className={`${styles.paddingX} bg-primary min-h-screen w-full flex-col flex items-center justify-center text-white `}
           >
-            <h1 className="text-[50px] xs:text-[80px] md:text-[90px] lg:text-[100px] 3xl:text-[120px] font-bold text-left xs:text-center">
+            <h1 className="text-[50px] xs:text-[80px] md:text-[90px] lg:text-[100px] 3xl:text-[120px] font-bold text-left md:text-center">
               HI, {`I'M`}{" "}
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
                 MD.MUZAHID
               </span>
             </h1>
-            <h1 className="text-[30px] xs:text-[50px] md:text-[60px] lg:text-[80px]  text-left xs:text-center mt-5 xs:mt-0">
+            <h1 className="text-[30px] xs:text-[50px] md:text-[60px] lg:text-[80px]  text-left md:text-center mt-5 xs:mt-0">
               I DEVELOP WEB WORLD, AND REACT NATIVE{" "}
               <br className="hidden 3xl:block" /> APPLICATION
+              <div className="text-[18px] mt-10 block xs:hidden">
+                <a
+                  href="#contact"
+                  type="button"
+                  className="transition ease-in-out delay-50 w-56  h-10 bg-gradient-to-r hover:-translate-y-1 duration-300 hover:scale-110 hover:shadow-lg active:scale-75 hover:shadow-red-500 from-indigo-500 via-purple-500 to-pink-500 font-bold text-white rounded-lg p-1"
+                >
+                  <div className="bg-black rounded-md w-full h-full flex items-center justify-center">
+                    <p>HIRE ME</p>
+                  </div>
+                </a>
+              </div>
             </h1>
           </div>
         </header>
 
         <div
           id="about"
-          className="min-h-screen bg-red-500 flex items-center justify-center text-white text-[120px] font-extrabold tracking-widest"
+          className="min-h-screen bg-red-500 flex items-center justify-center text-white text-[20px] font-extrabold tracking-widest"
         >
           <h1>ABOUT</h1>
         </div>
 
         <div
           id="portfolio"
-          className="min-h-screen bg-yellow-500 flex items-center justify-center text-white text-[120px] font-extrabold tracking-widest"
+          className="min-h-screen bg-yellow-500 flex items-center justify-center text-white text-[20px] font-extrabold tracking-widest"
         >
           <h1>PORTFOLIO</h1>
         </div>
 
         <div
           id="contact"
-          className="min-h-screen bg-indigo-500 flex items-center justify-center text-white text-[120px] font-extrabold tracking-widest"
+          className="min-h-screen bg-indigo-500 flex items-center justify-center text-white text-[20px] font-extrabold tracking-widest"
         >
           <h1>CONTACT</h1>
         </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,65 +11,67 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false, true);
 
   return (
-    <nav
-      className={`${styles.paddingX} bg-primary ${
-        toggle ? "bg-opacity-100" : "bg-opacity-100 xs:bg-opacity-0"
-      }
+    <div>
+      <nav
+        className={`${styles.paddingX} bg-primary ${
+          toggle ? "bg-opacity-100" : "bg-opacity-100 xs:bg-opacity-0"
+        }
       w-full flex items-center py-5 fixed top-0 z-50 `}
-    >
-      <div className="w-full max-w-7xl mx-auto flex justify-between items-center">
-        <Link
-          path="/"
-          className="flex items-center gap-2"
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
-        >
-          <figure className="w-9 h-9 object-contain">
-            <img src={logo} alt="Md.Muzahid" />
-          </figure>
-          <p className=" text-white text-[18px] font-bold cursor-pointer ">
-            Md. Muzahid
-          </p>
-        </Link>
-        <div>
-          <div className="flex items-center justify-center gap-4 md:gap-10">
-            {/* hire me button start here  */}
-            <a
-              href="#contact"
-              type="button"
-              className={`${
-                toggle ? "hidden" : "block"
-              } transition ease-in-out delay-50 w-28 h-9 bg-gradient-to-r hover:-translate-y-1 duration-300 hover:scale-110 hover:shadow-lg active:scale-75 hover:shadow-red-500 from-indigo-500 via-purple-500 to-pink-500 font-bold text-white rounded-lg p-1`}
-              onClick={() => setToggle(false)}
-            >
-              <div className="bg-black rounded-md w-full h-full flex items-center justify-center">
-                <p>HIRE ME</p>
-              </div>
-            </a>
-            {/* hire me button end here  */}
-
-            {/* menu toggle button start here */}
-            <button onClick={() => setToggle(!toggle)} className="menu-btn">
-              <motion.div
-                animate={{ top: toggle ? "-100%" : "0" }}
-                transition={{ duration: 0.5, ease: [0.85, 0, 0.15, 1] }}
-                className="slider"
+      >
+        <div className="w-full max-w-7xl mx-auto flex justify-between items-center">
+          <Link
+            path="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+          >
+            <figure className="w-9 h-9 object-contain">
+              <img src={logo} alt="Md.Muzahid" />
+            </figure>
+            <p className=" text-white text-[18px] font-bold cursor-pointer ">
+              Md. Muzahid
+            </p>
+          </Link>
+          <div>
+            <div className="flex items-center justify-center gap-4 md:gap-10">
+              {/* hire me button start here  */}
+              <a
+                href="#contact"
+                type="button"
+                className={`${
+                  toggle ? "xs:hidden" : "xs:block"
+                } transition ease-in-out hidden delay-50 w-28 h-9 bg-gradient-to-r hover:-translate-y-1 duration-300 hover:scale-110 hover:shadow-lg active:scale-75 hover:shadow-red-500 from-indigo-500 via-purple-500 to-pink-500 font-bold text-white rounded-lg p-1`}
+                onClick={() => setToggle(false)}
               >
-                <div className="el">
-                  <PerspectiveText label="MENU" />
+                <div className="bg-black rounded-md w-full h-full flex items-center justify-center">
+                  <p>HIRE ME</p>
                 </div>
-                <div className="el">
-                  <PerspectiveText label="CLOSE" />
-                </div>
-              </motion.div>
-            </button>
-            {/* menu toggle button end here */}
+              </a>
+              {/* hire me button end here  */}
+
+              {/* menu toggle button start here */}
+              <button onClick={() => setToggle(!toggle)} className="menu-btn">
+                <motion.div
+                  animate={{ top: toggle ? "-100%" : "0" }}
+                  transition={{ duration: 0.5, ease: [0.85, 0, 0.15, 1] }}
+                  className="slider"
+                >
+                  <div className="el">
+                    <PerspectiveText label="MENU" />
+                  </div>
+                  <div className="el">
+                    <PerspectiveText label="CLOSE" />
+                  </div>
+                </motion.div>
+              </button>
+              {/* menu toggle button end here */}
+            </div>
+            <DropDown toggle={toggle} setToggle={setToggle} />
           </div>
-          <DropDown toggle={toggle} setToggle={setToggle} />
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 

5e9aad48c8d88be6a9953ae2715cfd3ff240a043
MdMuzahid07 <mdmuzahid7396@gmail.com>
2023-12-17 20:12:36 +0600

    .env error fixed, yarn add react-firebase-hooks, google sign in button maked functional, dashboard logout button maked functinoal, Dashboard access blocked , and sign out automatically if the owner not valid to access dashboard, and Dashboard access validating

--- src/components/common/GoogleSignIn.jsx
+++ src/components/common/GoogleSignIn.jsx
@@ -1,14 +1,46 @@
 import { useNavigate } from "react-router-dom";
+import { useSignInWithGoogle, useSignOut } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useEffect } from "react";
+import Preloader from "../preloader/Preloader";
 
 const GoogleSignIn = () => {
   const navigate = useNavigate();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signOut, SignOutLoading, SignOutError] = useSignOut(auth);
+
+  useEffect(() => {
+    if (!loading && !error && user) {
+      if (
+        user?.user?.email === "mdmuzahid.dev@gmail.com" &&
+        user?.user?.displayName === "Md Muzahid"
+      ) {
+        navigate("/dashboard/home");
+      } else {
+        signOut();
+        window.alert(
+          "This button serves to authenticate the owner for Dashboard access. And you are not qualified as the legitimate owner of this website, you are automatically logged out."
+        );
+      }
+    }
+  }, [user, error, loading, navigate, signOut]);
+
+  if ((loading && !error && !user) || SignOutLoading) {
+    return <Preloader />;
+  }
+
+  if (!loading && !error && user) {
+    console.log(user);
+  }
+
+  if ((!loading && error && !user) || SignOutError) {
+    console.log((error, "error") || (SignOutError, "sing out error"));
+  }
 
   return (
     <div className="flex justify-center items-center min-h-screen w-full bg-primary">
       <button
-        onClick={() => {
-          navigate("/dashboard/home");
-        }}
+        onClick={() => signInWithGoogle()}
         className="transition ease-in-out delay-50 w-72 h-14 bg-gradient-to-r hover:-translate-y-1 duration-300 hover:scale-110 hover:shadow-lg active:scale-75 hover:shadow-red-500 from-indigo-500 via-purple-500 to-pink-500 font-bold text-white rounded-lg p-1"
       >
         <div className="bg-black rounded-md w-full h-full flex items-center justify-center">
--- src/layout/Dashboard/Header.jsx
+++ src/layout/Dashboard/Header.jsx
@@ -1,7 +1,26 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSignOut } from "react-firebase-hooks/auth";
+import Preloader from "../../components/preloader/Preloader";
+import auth from "../../firebase.init";
 
 /* eslint-disable react/prop-types */
 const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
+  const navigate = useNavigate();
+  const [signOut, SignOutLoading, SignOutError] = useSignOut(auth);
+
+  const handleOnclick = () => {
+    signOut();
+    setTimeout(navigate("/"), 3000);
+  };
+
+  if (SignOutLoading && !SignOutError) {
+    return <Preloader />;
+  }
+
+  if (!SignOutLoading && SignOutError) {
+    SignOutError, "sing out error";
+  }
+
   return (
     <header className="w-full h-20 bg-white sticky top-0 left-0 text-primary border-b px-7 flex justify-between items-center z-20">
       <button
@@ -10,7 +29,7 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen }) => {
       >
         {isSidebarOpen ? "Close" : "Open"}
       </button>
-      <Link to="/">Logout</Link>
+      <button onClick={handleOnclick}>Logout</button>
     </header>
   );
 };

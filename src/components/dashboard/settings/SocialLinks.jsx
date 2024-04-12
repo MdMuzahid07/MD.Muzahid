88bb9272b8f0664ff1770dd8e19e96244d16b6e3
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-04-12 22:54:34 +0600

    social link ui updated

--- src/components/dashboard/settings/SocialLinks.jsx
+++ src/components/dashboard/settings/SocialLinks.jsx
@@ -55,28 +55,40 @@ const SocialLinks = () => {
             <div className="w-[300px] max-h-80 h-80 px-4 py-5 bg-white border absolute md:right-0 bottom-20 overflow-y-auto">
               {connectMe?.map(({ name, icon }, index) => {
                 return (
-                  <div
-                    key={index}
-                    className="mb-4 w-full h-20 border px-4 py-2 bg-slate-50 flex items-center  gap-5 relative"
-                  >
-                    <img className="w-10 h-10" src={icon} alt="" />
-                    <p className="text-xl">{name}</p>
-                    <button title="Add Link" className="absolute right-4">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5 text-slate-400"
+                  <div key={index} className="border hover:border-red-500 mb-4">
+                    <div className="w-full h-20 px-4 py-2 bg-slate-50 flex items-center  gap-5 relative">
+                      <img className="w-10 h-10" src={icon} alt="" />
+                      <p className="text-xl">{name}</p>
+                      <span
+                        title="Add Link"
+                        className="absolute right-4 text-slate-400"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-                        />
-                      </svg>
-                    </button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                          />
+                        </svg>
+                      </span>
+                    </div>
+                    <div className="relative">
+                      <input
+                        placeholder="Add Link Here"
+                        className="border-t w-full  pl-3 focus:outline-none"
+                        type="text"
+                      />
+                      <button className="bg-red-500 text-white absolute right-1 text-xs px-3 bottom-1 top-1">
+                        Add
+                      </button>
+                    </div>
                   </div>
                 );
               })}

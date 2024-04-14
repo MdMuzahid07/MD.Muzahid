1377352177cd6a94b74f64363b30a1483ae196e5
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-04-14 18:19:59 +0600

    social link add ui updated, dashboard , settings

--- src/components/dashboard/settings/SocialLinks.jsx
+++ src/components/dashboard/settings/SocialLinks.jsx
@@ -3,6 +3,7 @@ import { facebook, github, linkedIn, reddit, twitter } from "../../../assets";
 
 const SocialLinks = () => {
   const [socialLinkAdd, setSocialLinkAdd] = useState(false);
+  const [addNewSocialPlatform, setAddNewSocialPlatform] = useState(false);
 
   const connectMe = [
     { name: "LinkedIn", icon: linkedIn, link: "mdmuzahid.dev", active: true },
@@ -11,6 +12,13 @@ const SocialLinks = () => {
     { name: "Github", icon: github, link: "mdmuzahid.dev", active: true },
     { name: "Facebook", icon: facebook, link: "mdmuzahid.dev", active: false },
   ];
+
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    setSocialLinkAdd(false);
+    setAddNewSocialPlatform(false);
+  };
 
   return (
     <div className="mt-10 w-full">
@@ -53,35 +61,63 @@ const SocialLinks = () => {
         <div className="relative">
           {socialLinkAdd && (
             <div className="w-[300px] max-h-96 h-96 px-4 py-5 bg-white border absolute md:right-0 bottom-20 overflow-y-auto">
-              {connectMe?.map(({ name, icon }, index) => {
-                return (
-                  <div key={index} className="border hover:border-red-500 mb-4">
-                    <div className="w-full h-20 px-4 py-2 bg-slate-50 flex items-center  gap-5 relative">
-                      <img className="w-10 h-10" src={icon} alt="" />
-                      <p className="text-xl">{name}</p>
-                      <span
-                        title="Add Link"
-                        className="absolute right-4 text-slate-400"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6"
+              {!addNewSocialPlatform &&
+                connectMe?.map(({ name, icon }, index) => {
+                  return (
+                    <div
+                      onClick={() => setAddNewSocialPlatform(true)}
+                      key={index}
+                      className="border hover:border-red-500 mb-4"
+                    >
+                      <div className="w-full h-20 px-4 py-2 bg-slate-50 flex items-center  gap-5 relative">
+                        <img className="w-10 h-10" src={icon} alt="" />
+                        <p className="text-xl">{name}</p>
+                        <span
+                          title="Add Link"
+                          className="absolute right-4 text-slate-400"
                         >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
-                          />
-                        </svg>
-                      </span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                            />
+                          </svg>
+                        </span>
+                      </div>
                     </div>
+                  );
+                })}
+
+              {addNewSocialPlatform && (
+                <div>
+                  <div className="flex justify-center mt-5">
+                    <img className="w-14 h-14" src={linkedIn} alt="" />
                   </div>
-                );
-              })}
+                  <p className="text-center mt-2 text-xl">LinkedIn</p>
+                  <form onSubmit={handleSave} className="mt-10">
+                    <input
+                      className="border w-full bg-slate-50 px-3 py-1 rounded-full focus:outline-none focus:border-indigo-500"
+                      type="text"
+                      name=""
+                      placeholder="Please enter the link "
+                      id=""
+                    />
+                    <div className="flex justify-center">
+                      <button className="w-24 mt-7 text-white bg-indigo-500 rounded-full">
+                        Save
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              )}
             </div>
           )}
           <button
